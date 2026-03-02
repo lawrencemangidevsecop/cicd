4a13517c7dfcc210acccb41a5b7d6bdfa8777269
lawrencemwangi496-design <lawrencemwangi496@gmail.com>
2026-03-02 14:36:50 +0300

    feature: shell check

---
+++ powermanagement.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+wait_time=15
+server_ip="192.168.0.117"
+
+
+while true;do
+    if acpi -a | grep -q "off-line" ; then
+
+wall "power lost ! server shutting down in  $wait_time seconds. type 'sudo shutdown -c' to stop it"
+
+sleep $wait_time
+  if acpi -a | grep -q "off-line"; then 
+    wall "power still out . killing processes and sycing disk..."
+    wall "pwer lost! gracefully shutting down any running containers...."
+    docker stop $(docker ps -q)
+    wall "container shut down successfully"
+    sleep 5
+
+     sync
+     sudo shutdown -h now
+     else
+
+     wall "power restored shutdown aborted"
+     fi
+fi
+sleep 5
+done
+
